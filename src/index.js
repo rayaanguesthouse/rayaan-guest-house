758bfa596c86b607c643655f7aac67a00f5a0aa4
rayaanguesthouse <rayaanguesthouse23@gmail.com>
2025-06-14 19:30:56 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import RayaanGuestHouse from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RayaanGuestHouse />
+  </React.StrictMode>
+);
